5138ba836cfbdf50b46c466335f17da68749c0eb
RoApostolova <zionbg@abv.bg>
2017-03-12 20:56:10 +0200

    Update DrunkenNumbers.cs

--- MentorMeeting-11-03-2017/DrunkenNumbers.cs
+++ MentorMeeting-11-03-2017/DrunkenNumbers.cs
@@ -4,61 +4,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-class DrunkenNumbers
+namespace DrunkenNumbers
 {
-    static void Main()
+    class DrunkenNumbers
     {
-        int numberRounds = int.Parse(Console.ReadLine());
-
-        int vladkoBeers = 0;
-        int mitkoBeers = 0;
-
-        for (int i = 0; i < numberRounds; i++)
+        static void Main()
         {
-            int drunkenNumber = int.Parse(Console.ReadLine());
+            int numberRounds = int.Parse(Console.ReadLine());
 
-            int numberDigits = 0;
-            int currentNumber = drunkenNumber;
-            while (currentNumber != 0)
-            {
-                currentNumber = currentNumber / 10;
-                numberDigits++;
-            }
-            //right part
-            for (int r = 0; r < numberDigits / 2; r++)
-            {
-                int currentDigit = drunkenNumber % 10;
-                drunkenNumber = drunkenNumber / 10;
-                vladkoBeers += currentDigit;
-            }
-            if (numberDigits % 2 != 0)
-            {
-                int middleDigit = drunkenNumber % 10;
-                mitkoBeers += middleDigit;
-                vladkoBeers += middleDigit;
-                drunkenNumber = drunkenNumber / 10;
-            }
-            //left part
-            for (int l = 0; l < numberDigits / 2; l++)
-            {
-                int currentDigit = drunkenNumber % 10;
-                drunkenNumber = drunkenNumber / 10;
-                mitkoBeers += currentDigit;
-            }
-        }
+            int vladkoBeers = 0;
+            int mitkoBeers = 0;
 
-        if (mitkoBeers == vladkoBeers)
-        {
-            Console.WriteLine("No {0}", vladkoBeers + mitkoBeers);
-        }
-        else if (mitkoBeers < vladkoBeers)
-        {
-            Console.WriteLine("V {0}", vladkoBeers - mitkoBeers);
-        }
-        else
-        {
-            Console.WriteLine("M {0}", mitkoBeers - vladkoBeers);
+            for (int i = 0; i < numberRounds; i++)
+            {
+                int drunkenNumber = int.Parse(Console.ReadLine());
+
+                int numberDigits = 0;
+                int currentNumber = drunkenNumber;
+                while (currentNumber != 0)
+                {
+                    currentNumber = currentNumber / 10;
+                    numberDigits++;
+                }
+                //right part
+                for (int r = 0; r < numberDigits / 2; r++)
+                {
+                    int currentDigit = drunkenNumber % 10;
+                    drunkenNumber = drunkenNumber / 10;
+                    vladkoBeers += currentDigit;
+                }
+                if (numberDigits % 2 != 0)
+                {
+                    int middleDigit = drunkenNumber % 10;
+                    mitkoBeers += middleDigit;
+                    vladkoBeers += middleDigit;
+                    drunkenNumber = drunkenNumber / 10;
+                }
+                //left part
+                for (int l = 0; l < numberDigits / 2; l++)
+                {
+                    int currentDigit = drunkenNumber % 10;
+                    drunkenNumber = drunkenNumber / 10;
+                    mitkoBeers += currentDigit;
+                }
+            }
+
+            if (mitkoBeers == vladkoBeers)
+            {
+                Console.WriteLine("No {0}", vladkoBeers + mitkoBeers);
+            }
+            else if (mitkoBeers < vladkoBeers)
+            {
+                Console.WriteLine("V {0}", vladkoBeers - mitkoBeers);
+            }
+            else
+            {
+                Console.WriteLine("M {0}", mitkoBeers - vladkoBeers);
+            }
         }
     }
 }
