17a85d7a4d99d3a906947599b274e3e2b8d69cef
RoApostolova <zionbg@abv.bg>
2017-03-12 20:53:18 +0200

    Create SumOfEvenDivisors.cs

---
+++ MentorMeeting-11-03-2017/SumOfEvenDivisors.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfEvenDivisors
+{
+    class SumOfEvenDivisors
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int sumDivisors = 0;
+
+            for (int i = start; i <= end; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0 && j % 2 == 0)
+                    {
+                        sumDivisors += j;
+                    }
+                }
+            }
+            Console.WriteLine(sumDivisors);
+        }
+    }
+}
